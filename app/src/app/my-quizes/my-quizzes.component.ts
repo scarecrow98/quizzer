98d316297998d6c6f159f040b0e75e931a44bd1d
Révész Ferenc <revesz.ferenc@govern.hu>
2022-11-03 22:41:11 +0100

    my-quiz list can be filtered

--- app/src/app/my-quizes/my-quizzes.component.ts
+++ app/src/app/my-quizes/my-quizzes.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, fromEvent, map, startWith, Subscription, withLatestFrom } from 'rxjs';
+import { Quiz } from '../core/models/quiz.model';
 import { QuizService } from '../core/services/quiz.service';
 
 @Component({
@@ -7,13 +9,41 @@ import { QuizService } from '../core/services/quiz.service';
   styleUrls: ['./my-quizzes.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MyQuizzesComponent implements OnInit {
+export class MyQuizzesComponent implements OnInit, OnDestroy {
 
-  quizzes$ = this.service.getQuizzes();
-  
-  constructor(private service: QuizService) { }
+  private _quizzes$ = new BehaviorSubject<Quiz[]>([]);
+
+  private _filteredQuizzes$ = new BehaviorSubject<Quiz[]>([]);
+
+  private _sub: Subscription | null = null;
+
+  @ViewChild('searchInput', { static: true }) searchInput!: ElementRef<HTMLInputElement>;
+
+  public filteredQuizzes$ = this._filteredQuizzes$.asObservable();
+
+  constructor(private service: QuizService) {
+    this.service.getQuizzes().subscribe(quizzes => {
+      this._quizzes$.next(quizzes);
+      this._filteredQuizzes$.next(quizzes);
+    });
+  }
 
   ngOnInit(): void {
+    this._sub = fromEvent(this.searchInput.nativeElement, 'keyup').pipe(
+      map((event: any) => this.searchInput.nativeElement.value.toLocaleLowerCase()),
+      debounceTime(1000),
+      distinctUntilChanged(),
+      withLatestFrom(this._quizzes$)
+    ).subscribe(([search, quizzes]) => {
+      this._filteredQuizzes$.next(
+        quizzes.filter(q => q.title.toLocaleLowerCase().includes(search))
+      );
+    });
   }
+
+  ngOnDestroy(): void {
+    this._sub?.unsubscribe();
+  }
+
 
 }
